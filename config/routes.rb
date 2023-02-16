ebe38801b86e158ddfc3a4497e202139ba59d8a4
tunghqh <tungcules1999@gmail.com>
2023-02-16 17:15:48 +0700

    test

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # TYPE = "c2".freeze
+  # # constraints subdomain: TYPE do
+  #   resources :posts
+  # # end
+  # TYPE2 = "c1".freeze
+
+  constraints subdomain: 'c' do
+    resources :posts
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
